0167f9d2304edb430aae0f867bd790c3ac2a8671
RafaelTeixeiraSerafim <rafael.teixeiraserafim@gmail.com>
2024-08-31 12:48:03 -0300

    Remoção dos arquivos da main

--- src/main/java/com/rafaelteixeiraserafim/tcc/weight/WeightRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rafaelteixeiraserafim.tcc.weight;
-
-import com.rafaelteixeiraserafim.tcc.product_item.ProductItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WeightRepository extends JpaRepository<ProductItem, Long> {
-}
