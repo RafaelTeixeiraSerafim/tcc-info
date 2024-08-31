3ec2ae6cb3c7bdc9ade731001d7f246d2946e7e3
RafaelTeixeiraSerafim <rafael.teixeiraserafim@gmail.com>
2024-08-31 12:48:03 -0300

    Remoção dos arquivos da main

--- src/main/java/com/rafaelteixeiraserafim/tcc/weight/WeightController.java
+++
@@ -1,8 +0,0 @@
-package com.rafaelteixeiraserafim.tcc.weight;
-
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping(path = "api/v1/")
-public class WeightController {
-}
--- src/main/java/com/rafaelteixeiraserafim/tcc/weight/WeightService.java
+++
@@ -1,7 +0,0 @@
-package com.rafaelteixeiraserafim.tcc.weight;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class WeightService {
-}
