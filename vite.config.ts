b984cc46f0996f278114500f4ef7f699d92faaee
RafaelTeixeiraSerafim <rafael.teixeiraserafim@gmail.com>
2024-10-01 23:40:32 -0300

    Change proxy pattern

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,10 +7,10 @@ export default defineConfig({
   base: "/tcc-info/",
   server: {
     proxy: {
-      '/api': {
+      "api/": {
         target: process.env.VITE_API_URL,
         changeOrigin: true,
       },
     },
   },
-})
+});
