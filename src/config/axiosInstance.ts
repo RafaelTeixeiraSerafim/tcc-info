5f67a9bc65e60e15c5a31698beb606a501c4de85
RafaelTeixeiraSerafim <rafael.teixeiraserafim@gmail.com>
2024-10-01 23:40:32 -0300

    Change proxy pattern

--- src/config/axiosInstance.ts
+++ src/config/axiosInstance.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({ baseURL: import.meta.env.VITE_API_URL, withCredentials: true });
+// const axiosInstance = axios.create({ baseURL: import.meta.env.VITE_API_URL, withCredentials: true });
+
+const axiosInstance = axios;
 
 export default axiosInstance;
