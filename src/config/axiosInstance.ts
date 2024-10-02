ea2f539b2ebfaaf2317ca0a77d2fb1895a011e39
RafaelTeixeiraSerafim <rafael.teixeiraserafim@gmail.com>
2024-10-02 00:01:37 -0300

    Change proxy back to custom axiosInstance

--- src/config/axiosInstance.ts
+++ src/config/axiosInstance.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-// const axiosInstance = axios.create({ baseURL: import.meta.env.VITE_API_URL, withCredentials: true });
-
-const axiosInstance = axios;
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+});
 
 export default axiosInstance;
--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/tcc-info/",
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://tcc-info-backend.sa-east-1.elasticbeanstalk.com",
-        changeOrigin: true,
-      },
-    },
-  },
 });
