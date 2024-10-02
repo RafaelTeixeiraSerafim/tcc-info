3717ac3181b8b3d91292b3d80058cfefaac8f07e
RafaelTeixeiraSerafim <rafael.teixeiraserafim@gmail.com>
2024-10-01 23:33:40 -0300

    Add api proxy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/tcc-info/"
+  base: "/tcc-info/",
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_URL,
+        changeOrigin: true,
+      },
+    },
+  },
 })
