f053410adfd931dda99e1b2c9e04fb35cd776eea
RafaelTeixeiraSerafim <rafael.teixeiraserafim@gmail.com>
2024-10-01 22:54:59 -0300

    Add real backend url

--- src/config/axiosInstance.tsx
+++ src/config/axiosInstance.tsx
@@ -1,11 +1,5 @@
 import axios from "axios";
 
-// For development
-const baseURL = "http://localhost:8080/";
-
-// For deployment
-// const baseURL = "https://backend-kqnasaoyha-uc.a.run.app";
-
-const axiosInstance = axios.create({ baseURL: baseURL, withCredentials: true });
+const axiosInstance = axios.create({ baseURL: import.meta.env.VITE_API_URL, withCredentials: true });
 
 export default axiosInstance;
