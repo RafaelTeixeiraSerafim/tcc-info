5df6307c3624147420803189a8b163dee6aa3fb7
RafaelTeixeiraSerafim <rafael.teixeiraserafim@gmail.com>
2024-10-05 23:10:20 -0300

    fix: change HashRouter back to BrowserRouter and add spa-github-pages boilerplate for redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import { Suspense } from "react";
 import LoadingFallback from "./components/LoadingFallback";
@@ -32,7 +32,7 @@ function App() {
     <ThemeProviderWrapper>
       <UserProvider>
         <Suspense fallback={<LoadingFallback />}>
-          <HashRouter basename="/tcc-info">
+          <BrowserRouter basename="/tcc-info">
             <ScrollToTop />
             <Routes>
               <Route path="/" element={<UserLayout />}>
@@ -96,7 +96,7 @@ function App() {
                 <Route path="*" element={<NoPage />} />
               </Route> */}
             </Routes>
-          </HashRouter>
+          </BrowserRouter>
         </Suspense>
       </UserProvider>
     </ThemeProviderWrapper>
