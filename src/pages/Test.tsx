37f4834dbe3cd522f822d0645974343b991b5b97
RafaelTeixeiraSerafim <rafael.teixeiraserafim@gmail.com>
2024-10-20 00:24:03 -0300

    New deploy to github pages

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 export default function Test() {
   const [obj, setObj] = useState({
