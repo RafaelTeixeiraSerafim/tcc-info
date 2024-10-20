ee7f92b2f8cf8fb87217a74d71051fd4cc337c9a
RafaelTeixeiraSerafim <rafael.teixeiraserafim@gmail.com>
2024-10-20 12:41:20 -0300

    Move files to backend/postal-service folder

--- app/routes/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.routes.address_routes import address_routes
-from app.routes.shipping_routes import shipping_routes
-
-def route_index(app):
-    address_routes(app)
-    shipping_routes(app)
