64f4d7491c27bb3f3fc73a7c354a4371708fc8d1
RafaelTeixeiraSerafim <rafael.teixeiraserafim@gmail.com>
2024-10-29 00:53:58 -0300

    Remove host in prod for eureka server connection in postal-service

--- backend/postal-service/app/__init__.py
+++ backend/postal-service/app/__init__.py
@@ -11,7 +11,6 @@ import os
 
 def create_app():
     app = Flask(__name__)
-    # env_config = os.getenv("APP_SETTINGS", "app.config.config.Config")
     app.config.from_object(Config)
     db.init_app(app)
     with app.app_context():
@@ -21,9 +20,11 @@ def create_app():
     migrate.init_app(app, db)
     if app.config.get("IS_PROD") == "True":
         host = app.config.get("EUREKA_SERVER_HOST")
+        server_url = f'http://{host}/eureka'
     else:
         host = get_windows_host_ip()
-    eureka_client.init(eureka_server=f'http://{host}:8761/eureka', app_name="postal-service", instance_port=5000, instance_ip="127.0.0.1")
+        server_url = f'http://{host}:8761/eureka'
+    eureka_client.init(eureka_server=server_url, app_name="postal-service", instance_port=app.config.get("FLASK_RUN_PORT"), instance_ip="127.0.0.1")
     CORS(app)
     route_index(app)
     return app
